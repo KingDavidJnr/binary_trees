4bb19c1f4425adf3da759d64f16c7a17ca575bff
David Oduse <89360467+KingDavidJnr@users.noreply.github.com>
2023-05-31 21:07:29 +0000

    added 5 file

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_root - Evaluates whether the node is the root of a binary tree.
+ * @node: The pointer to the node to be checked.
+ *
+ * Return: If the node is a root - 1.
+ *         Otherwise - 0.
+ */
+
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node == NULL || node->parent != NULL)
+		return (0);
+
+	return (1);
+}
