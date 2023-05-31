566d071feb84284bd2add5115b435b58602889ce
David Oduse <89360467+KingDavidJnr@users.noreply.github.com>
2023-05-31 22:07:33 +0000

    added 100 mtask

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+
+/**
+ * binary_trees_ancestor - Calc. the lowest common ancestor of two nodes.
+ * @first: Pointer to the 1st node.
+ * @second: Pointer to the 2nd node.
+ *
+ * Return: If no common ancestors return NULL, else return common ancestor.
+ */
+
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+		const binary_tree_t *second)
+{
+	binary_tree_t *mom, *pop;
+
+	if (!first || !second)
+		return (NULL);
+	if (first == second)
+		return ((binary_tree_t *)first);
+
+	mom = first->parent, pop = second->parent;
+	if (first == pop || !mom || (!mom->parent && pop))
+		return (binary_trees_ancestor(first, pop));
+	else if (mom == second || !pop || (!pop->parent && mom))
+		return (binary_trees_ancestor(mom, second));
+	return (binary_trees_ancestor(mom, pop));
+}
